a6e8a0eb062be8b2d2ca44887518fa6079994fbc
Philipp Flach <flach.philipp@luzi.ch>
2021-03-07 13:40:03 +0100

    Added JsonConvert.SerializeObject

--- ZPdbPlugIn/MapEntryManager.cs
+++ ZPdbPlugIn/MapEntryManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using Newtonsoft.Json;
 
 namespace ZPdbPlugIn
 {
@@ -30,12 +31,46 @@ namespace ZPdbPlugIn
         /// </summary>
         /// <returns>Current number of entries in the list</returns>
         [ComVisible(true)]
+        public string GetCurrentMarkersJSON()
+        {
+            string json = JsonConvert.SerializeObject(_mapEntries, Formatting.Indented);
+            return json;
+        }
+
+        /// <summary>
+        /// Add current map entry to the collection
+        /// </summary>
+        /// <returns>Current number of entries in the list</returns>
+        [ComVisible(true)]
         public int SaveMapEntry()
         {
             _mapEntries.Add(_currentMapEntry);
             _currentMapEntry = new MapEntry();
             return _mapEntries.Count;
         }
+
+        [ComVisible(true)]
+        public void SetID(long id) => _currentMapEntry.Id = id;
+
+        [ComVisible(true)]
+        public void SetMap_id(int map_id) => _currentMapEntry.Map_id = map_id;
+        [ComVisible(true)] 
+        public void SetAddress(string address) => _currentMapEntry.Address = address;
+
+        [ComVisible(true)]
+        public void SetDescription(string description) => _currentMapEntry.Description = description;
+
+        [ComVisible(true)]
+        public void SetLink(string link) => _currentMapEntry.Link = link;
+
+        [ComVisible(true)] 
+        public void SetLat(double? lat) => _currentMapEntry.Lat = lat;
+
+        [ComVisible(true)]
+        public void SetLng(double? lng) => _currentMapEntry.Lng = lng;
+
+        [ComVisible(true)]
+        public void SetTitle(string title) => _currentMapEntry.Title = title;
 
     }
 }
