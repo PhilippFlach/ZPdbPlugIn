e1561206b51fe47f5e8decbcad09c047749dfa30
Philipp Flach <flach.philipp@luzi.ch>
2021-03-07 10:00:44 +0100

    Add project files.

---
+++ ZPdbPlugIn/MapEntry.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ZPdbPlugIn
+{
+    internal class MapEntry
+    {
+        public MapEntry()
+        {
+        }
+
+        internal MapEntry(long id, int map_id, string address, string description, string pic, string link, string icon, double? lat, double? lng, string anim, string title, int infoopen, string category, int approved, int retina, int type, string did, int sticky, string other_data)
+        {
+            Id = id;
+            Map_id = map_id;
+            Address = address ?? throw new ArgumentNullException(nameof(address));
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+            Pic = pic ?? throw new ArgumentNullException(nameof(pic));
+            Link = link ?? throw new ArgumentNullException(nameof(link));
+            Icon = icon ?? throw new ArgumentNullException(nameof(icon));
+            Lat = lat;
+            Lng = lng;
+            Anim = anim ?? throw new ArgumentNullException(nameof(anim));
+            Title = title ?? throw new ArgumentNullException(nameof(title));
+            Infoopen = infoopen;
+            Category = category ?? throw new ArgumentNullException(nameof(category));
+            Approved = approved;
+            Retina = retina;
+            Type = type;
+            Did = did ?? throw new ArgumentNullException(nameof(did));
+            Sticky = sticky;
+            Other_data = other_data ?? throw new ArgumentNullException(nameof(other_data));
+        }
+
+        internal long Id { get; set; }
+        internal int Map_id { get; set; }
+        internal string Address { get; set; }
+        internal string Description { get; set; }
+        internal string Pic { get; set; }
+        internal string Link { get; set; }
+        internal string Icon { get; set; }
+        internal double? Lat { get; set; }
+        internal double? Lng { get; set; }
+        internal string Anim { get; set; }
+
+        internal string Title { get; set; }
+        internal int Infoopen { get; set; }
+        internal string Category { get; set; }
+        internal int Approved { get; set; }
+        internal int Retina { get; set; }
+        internal int Type { get; set; }
+
+        internal string Did { get; set; }
+        internal int Sticky { get; set; }
+        internal string Other_data { get; set; }
+  
+
+    }
+}
---
+++ ZPdbPlugIn/MapEntryManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ZPdbPlugIn
+{
+    /// <summary>    
+    /// Manages a collection of map entries that can be serialized to JSON <see cref="MapEntryManager"/>   
+    /// </summary>
+
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class MapEntryManager
+    {
+        List<MapEntry> _mapEntries = new List<MapEntry>();
+
+        MapEntry _currentMapEntry;
+
+        public MapEntryManager()
+        {
+            _currentMapEntry = new MapEntry();
+
+        }
+
+        /// <summary>
+        /// Add current map entry to the collection
+        /// </summary>
+        /// <returns>Current number of entries in the list</returns>
+        [ComVisible(true)]
+        public int SaveMapEntry()
+        {
+            _mapEntries.Add(_currentMapEntry);
+            return _mapEntries.Count;
+        }
+
+    }
+}
