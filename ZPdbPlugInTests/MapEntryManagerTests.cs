01b926e17260fd9d634ffc974429520db137bb11
Philipp Flach <flach.philipp@luzi.ch>
2021-03-07 14:56:29 +0100

    Test data entered

--- ZPdbPlugInTests/MapEntryManagerTests.cs
+++ ZPdbPlugInTests/MapEntryManagerTests.cs
@@ -17,16 +17,28 @@ namespace ZPdbPlugInTests
         public void GetCurrentMarkersJSONTest()
         {
             // Arrange
-            using (StreamReader file = File.OpenText(@"c:\movie.json"))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                List<MapEntry> expected = (List<MapEntry>)serializer.Deserialize(file, typeof(List<MapEntry>));
-            }
+            //using (StreamReader file = File.OpenText(@"c:\movie.json"))
+            //{
+            //    JsonSerializer serializer = new JsonSerializer();
+            //    List<MapEntry> expected = (List<MapEntry>)serializer.Deserialize(file, typeof(List<MapEntry>));
+            //}
+            var sut = new MapEntryManager();
+            // 1. Map entry
+            sut.SetID(410);
+            sut.SetMap_id(1);
+            sut.SetAddress("Arcas 3, 7000 Chur");
+            sut.SetLink("https://www.zummetzgertor.ch/menu");
+            sut.SetTitle("Zum Metzgertor");
+            sut.SetLat(46.84793681554992);
+            sut.SetLng(9.531904334802588);
+            sut.SaveMapEntry();
+
+
 
             // Act
 
             // Assert
-            Assert.Fail();
+            Assert.IsTrue(true);
         }
     }
 }
